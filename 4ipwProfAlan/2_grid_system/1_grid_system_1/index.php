5506d9d17e515c2eddb849e574d105c18593265e
Prof Alan Reis <98103226+ReiSalan94@users.noreply.github.com>
2023-02-24 22:14:34 -0300

    Add files via upload

--- 4ipwProfAlan/2_grid_system/1_grid_system_1/index.php
+++ 4ipwProfAlan/2_grid_system/1_grid_system_1/index.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/style.css" type="text/css">
+<link rel="stylesheet" href="css/style.css" type="text/css">
     <title>Olá, mundo!</title>
   </head>
   <body>
@@ -18,6 +18,14 @@
 				<header>
 					Header Cabeçalho
 				</header>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-4">
+				<nav> Menu </nav>
+			</div>
+			<div class="col-8">
+				<main>Conteúdo</main>
 			</div>
 		</div>
 	</div>
