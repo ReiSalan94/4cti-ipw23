d675d54f913203dadeeb55de1b60c5a55718d063
Prof Alan Reis <98103226+ReiSalan94@users.noreply.github.com>
2023-03-10 21:53:43 -0300

    Add files via upload

---
+++ 4ipwProfAlan/2_grid_system/4_grid_system_mobile/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Olá, mundo!</title>
+  </head>
+  <body>
+	
+	<div class="container">
+		<div class="row">	
+<!-- EX1) crie duas colunas que na resolução small(col-sm-?)
+ se divida em 8 e 4 colunas e resolução mobile 
+ ocupem 100% do espaço(col-12). 
+ -->
+	<div class="col-sm-8">Col 1</div>
+	<div class="col-sm-4">Col 2</div>
+		</div>
+		
+		
+			<!-- 
+			EX2) Crie três divs que na resulção md,
+			a primeira div ocupe o espaço 2 colunas,
+			a segunda div ocupe o espaço 4 colunas e
+			a terceira div ocupe o espaço 4 colunas.
+			No design mobile ambas ocuparação o espaço de 4 colunas.
+		
+			-->
+		<div class="row">
+			<div class="col-4 col-md-2">col 1</div>
+			<div class="col-4 col-md-4">col 2</div>
+			<div class="col-4 col-md-4">col 1</div>
+		</div>
+		
+		
+	</div>
+
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
